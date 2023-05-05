8b9267e46f3c0d4e58ea526cf8b2ade9d8ebd43e
LAbeast021 <kiarashhesampour@gmail.com>
2023-05-05 13:48:28 -0700

    working on cloudinary problem

--- routes/posts.js
+++ routes/posts.js
@@ -1,17 +1,21 @@
 var express = require('express');
 var router = express.Router();
-var User = require('../models/user')
-var Post = require('../models/post')
-var multer = require('multer');
-var cloudinary = require('cloudinary');
-var cloudinaryStorage = require('multer-storage-cloudinary');
-// var storage = cloudinaryStorage({
-//     cloudinary, 
+
+var User = require('../models/user');
+var Post = require('../models/post');
+
+// var multer = require('multer');
+// var cloudinary = require('cloudinary').v2;
+// var {cloudinaryStorage} = require('multer-storage-cloudinary');
+
+// var storage = new cloudinaryStorage({
+//     cloudinary : cloudinary, 
 //     filename: function (req, file, cb) {
 //         cb(null, new Date().toISOString() + file.originalname );
 //       },
 //     allowedFormats: ["jpg", "jpeg", "png"],
 // });
+
 // var upload = multer({storage})
 // var postsCtrl = require('../controllers/posts');
 
@@ -21,7 +25,8 @@ router.get('/new' ,function(req,res,next) {
     })
 })
 router.post('/upload' , function(req,res,next) {
-    console.log(req);
+    console.log(req.file);
+    res.redirect(`/users/profile/${req.user.id}`);
 })
 
 
