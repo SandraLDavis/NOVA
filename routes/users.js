7f5dde875e1437d154093b47b6e5a0d04e701f8c
LAbeast021 <kiarashhesampour@gmail.com>
2023-05-10 15:45:49 -0700

    going back to find the problem

--- routes/users.js
+++ routes/users.js
@@ -3,24 +3,20 @@ var router = express.Router();
 var User = require('../models/user');
 var Post = require('../models/post');
 var usersCtrl = require('../controllers/users');
-const mongoose = require('mongoose');
 
 
-router.get('/profile/:id' ,async function(req,res,next) {
-    // await Post.find({ userId : req.params.id}).exec((posts) => {;
+router.get('/profile/:id' , function(req,res,next) {
+    Post.find({userId:req.params.id}).then((posts) => {
+        console.log(posts);
         if (req.user.id == req.params.id) {
-            // var posts = await Post.find({userId : req.params.id});
-                // console.log(posts , " AVALIN BAR KE PEIDA SHODE");
-                res.render('./users/myProfile' , {
-                    name : req.user.firstName, 
-                    imge : req.user.avatar,
-                    loggedInUser : req.user , 
-                })
+            res.render('./users/myProfile.ejs' , {
+                loggedInUser : req.user
+        
+            })
         }
         else {
-            console.log(" chera oomade bashim to else akhe ????? ");
             res.render('./users/profile.ejs');
         }
-   
+    })
 })
 module.exports = router;
