68bc2a44bf4c06f97922332b2f9acf20dbc48457
LAbeast021 <kiarashhesampour@gmail.com>
2023-05-10 14:47:29 -0700

    went back but still doesnt work

--- routes/users.js
+++ routes/users.js
@@ -3,23 +3,24 @@ var router = express.Router();
 var User = require('../models/user');
 var Post = require('../models/post');
 var usersCtrl = require('../controllers/users');
+const mongoose = require('mongoose');
 
 
-router.get('/profile/:id' , function(req,res,next) {
-    
-    if (req.user.id == req.params.id) {
-        Post.find({userId : req.params.id}).then((posts) => {
-            res.render('./users/myProfile.ejs' , {
-                name : req.user.firstName, 
-                imge : req.user.avatar,
-                loggedInUser : req.user , 
-                posts
-        
-            })
-        })
-    }
-    else {
-        res.render('./users/profile.ejs');
-    }
+router.get('/profile/:id' ,async function(req,res,next) {
+    // await Post.find({ userId : req.params.id}).exec((posts) => {;
+        if (req.user.id == req.params.id) {
+            // var posts = await Post.find({userId : req.params.id});
+                // console.log(posts , " AVALIN BAR KE PEIDA SHODE");
+                res.render('./users/myProfile' , {
+                    name : req.user.firstName, 
+                    imge : req.user.avatar,
+                    loggedInUser : req.user , 
+                })
+        }
+        else {
+            console.log(" chera oomade bashim to else akhe ????? ");
+            res.render('./users/profile.ejs');
+        }
+   
 })
 module.exports = router;
