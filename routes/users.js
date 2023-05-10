a3d6431f9972de56e7ce035f642139c187959c82
LAbeast021 <kiarashhesampour@gmail.com>
2023-05-10 13:29:06 -0700

    recieving posts array from database for the login user

--- routes/users.js
+++ routes/users.js
@@ -1,17 +1,22 @@
 var express = require('express');
 var router = express.Router();
 var User = require('../models/user');
+var Post = require('../models/post');
 var usersCtrl = require('../controllers/users');
 
 
 router.get('/profile/:id' , function(req,res,next) {
     
     if (req.user.id == req.params.id) {
-        res.render('./users/myProfile.ejs' , {
-            name : req.user.firstName, 
-            imge : req.user.avatar,
-            loggedInUser : req.user
-    
+        Post.find({userId : req.params.id}).then((posts) => {
+            console.log(posts);
+            res.render('./users/myProfile.ejs' , {
+                name : req.user.firstName, 
+                imge : req.user.avatar,
+                loggedInUser : req.user , 
+                posts
+        
+            })
         })
     }
     else {
