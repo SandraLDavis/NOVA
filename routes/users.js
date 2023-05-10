a1f5dbc4216c9d693f373be60974fbeeac913634
LAbeast021 <kiarashhesampour@gmail.com>
2023-05-10 16:04:46 -0700

    profile page completed

--- routes/users.js
+++ routes/users.js
@@ -7,10 +7,11 @@ var usersCtrl = require('../controllers/users');
 
 router.get('/profile/:id' , function(req,res,next) {
     Post.find({userId:req.params.id}).then((posts) => {
-        console.log(posts);
+        var userPosts = posts ;
         if (req.user.id == req.params.id) {
             res.render('./users/myProfile.ejs' , {
-                loggedInUser : req.user
+                loggedInUser : req.user, 
+                posts : userPosts,
         
             })
         }
